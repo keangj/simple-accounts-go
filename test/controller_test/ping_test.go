078645ff4af3bbee18da5c1fb9d78fa37b35b773
jay <keangjay@gmail.com>
2023-04-02 23:49:07 +0800

    添加测试
    添加测试

---
+++ test/controller_test/ping_test.go
@@ -0,0 +1,26 @@
+package controller_test
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"simple-accounts/internal/router"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPing(t *testing.T) {
+	// Setup
+	r := router.New()
+
+	// Assertions
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body.String(), w.Code)
+	log.Fatalln(w.Body.String(), w.Code)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
+}
