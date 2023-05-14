1f52ff1ed49fbd7db8ab221fae67ec471374198b
jay <keangjay@gmail.com>
2023-05-14 23:38:57 +0800

    把 email 配置放到  env 中

--- internal/email/email.go
+++ internal/email/email.go
@@ -2,8 +2,17 @@ package email
 
 import (
 	"log"
+	"os"
+	"strconv"
 
 	gomail "gopkg.in/gomail.v2"
+)
+
+var (
+	EMAIL_SMTP_HOST = os.Getenv("EMAIL_SMTP_HOST")
+	EMAIL_SMTP_PORT = os.Getenv("EMAIL_SMTP_PORT")
+	EMAIL_USER      = os.Getenv("EMAIL_SMTP_USER")
+	EMAIL_PWD       = os.Getenv("EMAIL_SMTP_PWD")
 )
 
 func Send() {
@@ -15,10 +24,13 @@ func Send() {
 	m.SetHeader("Subject", "Hello!")
 	m.SetBody("text/html", "Hello <b>test</b> and <i>jay</i>!")
 	// m.Attach("/home/Alex/lolcat.jpg")
-
-	d := gomail.NewDialer("smtp.qq.com", 465, "1187688808@qq.com", "")
-	// d := gomail.NewDialer("smtp.gmail.com", 587, "keangjay@gmail.com", "")
-
+	var d *gomail.Dialer
+	if port, err := strconv.Atoi(EMAIL_SMTP_PORT); err != nil {
+		log.Fatalln(err)
+	} else {
+		d = gomail.NewDialer(EMAIL_SMTP_HOST, port, EMAIL_USER, EMAIL_PWD)
+		// d := gomail.NewDialer("smtp.gmail.com", 587, "keangjay@gmail.com", "")
+	}
 	// Send the email to Bob, Cora and Dan.
 	if err := d.DialAndSend(m); err != nil {
 		log.Println(err)
